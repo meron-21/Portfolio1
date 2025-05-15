0242ec9613f54cf24b19d4927c8eefcf17c992a0
meron-21 <meronmesfin1219@gmail.com>
2025-05-15 13:02:45 +0300

    portfolio

---
+++ MeronPortfolio/src/constants/index.js
@@ -0,0 +1,230 @@
+import { meta, shopify, starbucks, tesla } from "../assets/images";
+import {
+    car,
+    contact,
+    css,
+    estate,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    typescript,
+
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    /*{
+        imageUrl: express,
+        name: "Express",
+        type: "Backend",
+    };*/
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    /*{
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    {
+        imageUrl: motion,
+        name: "Motion",
+        type: "Animation",
+    },
+    {
+        imageUrl: mui,
+        name: "Material-UI",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nextjs,
+        name: "Next.js",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    
+    {
+        imageUrl: redux,
+        name: "Redux",
+        type: "State Management",
+    },
+    {
+        imageUrl: sass,
+        name: "Sass",
+        type: "Frontend",
+    },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: typescript,
+        name: "TypeScript",
+        type: "Frontend",
+    }*/
+];
+
+export const experiences = [
+    {
+        title: "Html & CSS Developer",
+        company_name: "",
+        icon: html,
+        iconBg: "#accbe1",
+        date: "",
+        points: [
+            "",
+            "",
+            "",
+            "",
+        ],
+    },
+    {
+        title: " javascript Developer",
+        company_name: "",
+        icon: javascript,
+        iconBg: "#fbc3bc",
+        date: "",
+        points: [
+            "",
+            "",
+            "",
+            "",
+        ],
+    },
+    {
+        title: "Web Developer",
+        company_name: "",
+        icon: react,
+        iconBg: "#b7e4c7",
+        date: "",
+        points: [
+            "",
+            "",
+            "",
+            "",
+        ],
+    },
+    /*{
+        title: "Full stack Developer",
+        company_name: "",
+        icon: meta,
+        iconBg: "#a2d2ff",
+        date: "",
+        points: [
+            "",
+            "",
+            "",
+            "",
+        ],
+    },*/
+];
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/meron-21',
+    },
+    /*{
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/YourLinkedInUsername',
+    }*/
+];
+
+export const projects = [
+    /*{
+        iconUrl: pricewise,
+        theme: 'btn-back-red',
+        name: 'Lorem Ipsum Project 1',
+        description: 'A cutting-edge application designed to simplify complex workflows and enhance productivity.',
+        link: 'https://github.com',
+    },
+    {
+        iconUrl: threads,
+        theme: 'btn-back-green',
+        name: 'Lorem Ipsum Project 2',
+        description: 'An innovative platform enabling seamless collaboration and communication across teams.',
+        link: 'https://github.com',
+    },
+    {
+        iconUrl: car,
+        theme: 'btn-back-blue',
+        name: 'Lorem Ipsum Project 3',
+        description: 'A user-friendly tool for managing and organizing daily tasks efficiently.',
+        link: 'https://github.com',
+    },
+    {
+        iconUrl: snapgram,
+        theme: 'btn-back-pink',
+        name: 'Lorem Ipsum Project 4',
+        description: 'A robust solution for sharing and discovering creative content in a social environment.',
+        link: 'https://github.com',
+    },
+    {
+        iconUrl: estate,
+        theme: 'btn-back-black',
+        name: 'Lorem Ipsum Project 5',
+        description: 'A comprehensive platform for exploring and managing real estate properties.',
+        link: 'https://github.com',
+    },*/
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-yellow',
+        name: 'Lorem Ipsum Project 6',
+        description: 'An AI-powered application that generates concise summaries from extensive text content.',
+        link: 'https://github.com',
+    }
+];
